4fba9bf4e3d68cbdf7e71e984b71c0a39f9bd65b
Rishab Raj <rr0948555@gmail.com>
2026-09-16 15:17:44 +0530

    Selectors - Reading State

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
+import { counterReducer } from './store/counter.reducer';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes), provideStore()],
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes),
+  provideStore({counter: counterReducer})],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,22 @@
 import { Component} from '@angular/core';
 import { Store } from '@ngrx/store';
 import { decrement, increment, reset } from './store/counter.actions';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { AppState, selectCounter } from './store/counter.selectors';
 
 @Component({
-  imports: [],
+  imports: [CommonModule],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
-   constructor(private store:Store){}
+
+   counter$!: Observable<number>;
+   constructor(private store:Store<AppState>){
+       this.counter$ = this.store.select(selectCounter);
+   }
 
    inc(){
      this.store.dispatch(increment());
