b1f90e8f99b9e8041859d1e370ced3e0d05631a8
Rishab Raj <rr0948555@gmail.com>
2026-09-16 14:53:16 +0530

    Actions

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component} from '@angular/core';
+import { Store } from '@ngrx/store';
+import { decrement, increment, reset } from './store/counter.actions';
 
 @Component({
   imports: [],
@@ -7,6 +9,17 @@ import { Component, signal } from '@angular/core';
   templateUrl: './app.html',
 })
 export class App {
-   count = 0;
-   user = [];
+   constructor(private store:Store){}
+
+   inc(){
+     this.store.dispatch(increment());
+   }
+
+   dec(){
+     this.store.dispatch(decrement());
+   }
+
+   resetCount(){
+      this.store.dispatch(reset());
+   }
 }
