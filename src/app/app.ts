783e3bbbeda7fc1363546378647b8d7e36c91bb0
Rishab Raj <rr0948555@gmail.com>
2026-09-16 14:38:16 +0530

    Ngrx

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,4 @@
 import { Component, signal } from '@angular/core';
-import { Review, ReviewService } from './review-service';
 
 @Component({
   imports: [],
@@ -8,40 +7,6 @@ import { Review, ReviewService } from './review-service';
   templateUrl: './app.html',
 })
 export class App {
-    reviews: Review[] = []
-    currentIndex = signal(0);
-    loading = signal(true);
-
-    constructor(private reviewService:ReviewService) {}
-
-    ngOnInit(){
-      this.reviewService.getReviews().subscribe((data) => {
-          this.reviews = data;
-          this.loading.set(false);
-      })
-    }
-
-    get currentReview(): Review | null {
-       return this.reviews.length ? this.reviews[this.currentIndex()] : null ;
-    }
-
-    nextReview(){
-       let index = this.currentIndex() + 1;
-       if(index >= this.reviews.length) {
-          index = 0;
-       }
-       this.currentIndex.set(index);
-    }
-
-     previousReview(){
-      let index = this.currentIndex() - 1;
-      if(index < 0){
-         index = this.reviews.length - 1;
-      }
-      this.currentIndex.set(index);
-    }
-    randomReview(){
-       let index = Math.floor(Math.random() * this.reviews.length);
-       this.currentIndex.set(index);
-    }
+   count = 0;
+   user = [];
 }
